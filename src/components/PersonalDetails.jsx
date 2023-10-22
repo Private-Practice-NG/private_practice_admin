a9f54a48a1b89efa6c0e0e5de762fa61603bfbdb
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-10-22 19:23:47 +0100

    feat: created the personal details component

---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import "./styles/personaldetails.css"
+const PersonalDetails = () => {
+  return (
+    <div className='personal-details'>
+        <div className="person-details-info">
+            <h3>Personal Information</h3>
+            <div className="personal-details-info-fields">
+                <div className="personal-details-info-field">
+                    <h4>FULL NAME</h4>
+                    <p>David Paul James</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>PHONE NUMBER</h4>
+                    <p>07060780922</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>EMAIL ADDRESS</h4>
+                    <p>davidpaul@gmail.com</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>BVN</h4>
+                    <p>davidpaul@gmail.com</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>GENDER</h4>
+                    <p>Male</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>MARITAL STATUS</h4>
+                    <p>Single</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>CHILDREN</h4>
+                    <p>None</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>TYPE OF RESIDENCE</h4>
+                    <p>Personal Apartment</p>
+                </div>
+            </div>
+        </div>
+        <div className="person-details-info">
+            <h3>Education and Employment</h3>
+            <div className="personal-details-info-fields">
+                <div className="personal-details-info-field">
+                    <h4>level of education</h4>
+                    <p>B.Sc</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>employment status</h4>
+                    <p>Self Employed</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>sector of employment</h4>
+                    <p>Medical</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>office email</h4>
+                    <p>davidpaul@privatepractice.com</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>Monthly income</h4>
+                    <p>₦200,000.00</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>Duration of employment</h4>
+                    <p>3 Months</p>
+                </div>
+            </div>
+        </div>
+        <div className="person-details-info">
+            <h3>Socials</h3>
+            <div className="personal-details-info-fields">
+                <div className="personal-details-info-field">
+                    <h4>Twitter</h4>
+                    <p>@davidthedoc</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>Facebook</h4>
+                    <p>David Paul</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>Instagram</h4>
+                    <p>@davidthedoc</p>
+                </div>
+            </div>
+        </div>
+        <div className="person-details-info">
+            <h3>Guarantor</h3>
+            <div className="personal-details-info-fields">
+                <div className="personal-details-info-field">
+                    <h4>FULL NAME</h4>
+                    <p>Thomas John</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>PHONE NUMBER</h4>
+                    <p>07060780922</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>EMAIL ADDRESS</h4>
+                    <p>Thomasjohn234@gmail.com</p>
+                </div>
+                <div className="personal-details-info-field">
+                    <h4>Relationship</h4>
+                    <p>Uncle</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PersonalDetails
