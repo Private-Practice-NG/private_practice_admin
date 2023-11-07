de0622d8659ab7b43accf6c2d468816a9b364fcf
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-11-07 08:07:19 +0100

    feat: created the route for protected added the provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import ForgotPassword from "./pages/ForgetPassword";
 import ForgotPassMsg from "./pages/ForgotPassMsg";
-import Navbars from "./components/PageContainer";
+import Layout from "./components/PageContainer";
+import Home from "./pages/Home";
+import Admins from "./pages/Admins";
+import Specialists from "./pages/Specialists";
+import Hospitals from './pages/Hospitals'
+import SpecialistDetails from "./components/SpecialistDetails";
+import PrivateRoute from "./components/PrivateRoute";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Auth />} />
+          <Route path="/login" element={<Auth />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/forgot-pass-msg" element={<ForgotPassMsg />} />
-          <Route path="/page-cont" element={<Navbars />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<PrivateRoute />}>
+              <Route index element={<Home />} />
+              <Route path="/admins" element={<Admins />} />
+              <Route path="/hospitals" element={<Hospitals />} />
+              <Route path="/specialists" element={<Specialists />} />
+              <Route path="/specialist/:userId" element={<SpecialistDetails />} />
+              <Route path="/hospital/:userId" element={<SpecialistDetails />} />
+            </Route>
+          </Route>
         </Routes>
+        <ToastContainer />
       </BrowserRouter>
     </>
   );
