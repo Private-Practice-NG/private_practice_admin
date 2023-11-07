61e2096f35637b8e8ba1983740fe7cf626396101
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-11-07 08:13:00 +0100

    feat: modified the component

--- src/pages/Specialists.jsx
+++ src/pages/Specialists.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/userstab.css";
 import { CiSearch } from "react-icons/ci";
-import UserProfileCard from "./UserProfileCard";
-const UsersTab = () => {
+import UserProfileCard from "../components/UserProfileCard";
+const Specialists = () => {
   return (
     <div className="users-tab">
       <header className="users-tab-header">
-        <h1>Hospitals</h1>
+        <h1>Specialist</h1>
         <div className="users-tab-input">
           <CiSearch />
-          <input type="text" placeholder="Search Hospital" />
+          <input type="text" placeholder="Search Specialist" />
         </div>
         <div className="users-tab-sort">
           <svg
@@ -22,38 +22,38 @@ const UsersTab = () => {
             <path
               d="M0 2H29.2582"
               stroke="#292D32"
-              stroke-width="2.5"
-              stroke-linecap="round"
+              strokeWidth="2.5"
+              strokeLinecap="round"
             />
             <path
               d="M4.87646 10.1273H24.382"
               stroke="#292D32"
-              stroke-width="2.5"
-              stroke-linecap="round"
+              strokeWidth="2.5"
+              strokeLinecap="round"
             />
             <path
               d="M11.3782 18.2546H17.88"
               stroke="#292D32"
-              stroke-width="2.5"
-              stroke-linecap="round"
+              strokeWidth="2.5"
+              strokeLinecap="round"
             />
           </svg>
           <p>Sort</p>
         </div>
       </header>
       <section className="users-tab-profiles">
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
-        <UserProfileCard />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
+        <UserProfileCard user='specialist' />
 
       </section>
     </div>
   );
 };
 
-export default UsersTab;
+export default Specialists;
