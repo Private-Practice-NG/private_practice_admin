a66fb058777ec1bcd39e30f858fb22dbe7fe03c7
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-11-12 07:29:34 +0100

    Updated the redux api and Reducer

--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -8,12 +8,29 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
-        
       }),
     }),
     dashboard: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/dashboard`,
+        method: "GET",
+      }),
+    }),
+    specialists: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/specialists`,
+        method: "GET",
+      }),
+    }),
+    hospitals: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/hospitals`,
+        method: "GET",
+      }),
+    }),
+    admins: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/admins`,
         method: "GET",
       }),
     }),
@@ -23,8 +40,35 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: "POST",
       }),
     }),
+    specialist: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/specialist?id=${userId}`,
+        method: "GET",
+      }),
+    }),
+    specialist: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/specialist?id=${userId}`,
+        method: "GET",
+      }),
+    }),
+    activateAdmin: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/activate`,
+        method: "PUT",
+        body:data
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useDashboardMutation } =
-  usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useDashboardMutation,
+  useSpecialistsMutation,
+  useHospitalsMutation,
+  useAdminsMutation,
+  useSpecialistMutation,
+  useActivateAdminMutation
+} = usersApiSlice;
