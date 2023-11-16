6f2000c2abd94640c2b2350aa3d181f805023fec
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-11-16 07:54:20 +0100

    feat:no credentials

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -3,8 +3,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BASE_URL,
-  credentials: "include",
-  mode:"no-cors"
+  // credentials: "include",
 });
 
 export const apiSlice = createApi({
