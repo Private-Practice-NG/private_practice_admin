f61bc00fcbfc9a551f7d7ed12482077e04fb72b9
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-11-16 03:50:58 +0100

    fat: update cors

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -4,6 +4,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BASE_URL,
   credentials: "include",
+  mode:"no-cors"
 });
 
 export const apiSlice = createApi({
