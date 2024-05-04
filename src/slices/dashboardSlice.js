144e7e5b01eb0509922d40b1973ea63ae30f91e3
Okpainmo <okpainmoandrew@gmail.com>
2024-05-04 04:44:13 +0100

    refactor: redux toolkit updates

--- src/slices/dashboardSlice.js
+++ src/slices/dashboardSlice.js
@@ -6,19 +6,19 @@ const initialState = {
 }
 
 const dashboardSlice = createSlice({
-    name:"dashboard",
-    initialState,
-    reducers:{
-        setDashboard:(state, action) =>{
-            state.dashboardInfo = action.payload;
-            localStorage.setItem('dashboardInfo',JSON.stringify(action.payload))
-        },
-        removeDashboard:(state, action) =>{
-            state.dashboardInfo = null;
-            localStorage.removeItem('dashboardInfo')
-        }
-    }
-})
+  name: 'dashboard',
+  initialState,
+  reducers: {
+    setDashboard: (state, action) => {
+      state.dashboardInfo = action.payload;
+      localStorage.setItem('dashboardInfo', JSON.stringify(action.payload));
+    },
+    removeDashboard: (state) => {
+      state.dashboardInfo = null;
+      localStorage.removeItem('dashboardInfo');
+    },
+  },
+});
 
 
 export const {setDashboard, removeDashboard} = dashboardSlice.actions
