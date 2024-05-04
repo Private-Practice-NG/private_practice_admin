0888b2a8ef4d68acf9663e1a39802f34b9b99b2d
Okpainmo <okpainmoandrew@gmail.com>
2024-05-04 04:46:57 +0100

    chore: removed the  netlify config file from project + more

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Auth from "./pages/Auth";
-import ForgotPassword from "./pages/ForgetPassword";
-import ForgotPassMsg from "./pages/ForgotPassMsg";
-import Layout from "./components/PageContainer";
-import Home from "./pages/Home";
-import Admins from "./pages/Admins";
-import Specialists from "./pages/Specialists";
-import Hospitals from './pages/Hospitals'
-import SpecialistDetails from "./components/SpecialistDetails";
-import Hospital from "./components/Hospital";
-import NotFound from "./pages/NotFound";
-import PrivateRoute from "./components/PrivateRoute";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+// import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import ForgotPasswordMessage from './pages/ForgotPasswordMessage';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Admins from './pages/Admins';
+import Specialists from './pages/Specialists';
+import Hospitals from './pages/Hospitals';
+import SpecialistDetails from './components/SpecialistDetails';
+import Hospital from './components/Hospital';
+import NotFound from './pages/NotFound';
+// import PrivateRoute from './components/PrivateRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CreateAdmin from './pages/Admins/components/CreateAdmin';
+import JobsPage from './pages/Jobs';
+import HospitalsEnrolment from './pages/HospitalsEnrolment';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Auth />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/forgot-pass-msg" element={<ForgotPassMsg />} />
-          
+          <Route
+            path="/forgot-password-message"
+            element={<ForgotPasswordMessage />}
+          />
+
           <Route path="/" element={<Layout />}>
-            <Route path="" element={<PrivateRoute />}>
-              <Route index element={<Home />} />
-              <Route path="/admins" element={<Admins />} />
-              <Route path="/hospitals" element={<Hospitals />} />
-              <Route path="/specialists" element={<Specialists />} />
-              <Route path="/specialist/:userId" element={<SpecialistDetails />} />
-              <Route path="/hospital/:userId" element={<Hospital />} />
-            </Route>
+            {/* <Route path="" element={<PrivateRoute />}> */}
+            <Route index element={<Home />} />
+            <Route path="/admins" element={<Admins />} />
+            <Route path="/jobs" element={<JobsPage />} />
+            <Route
+              path="/admins/create-admin-account"
+              element={<CreateAdmin />}
+            />
+            <Route path="/hospitals" element={<Hospitals />} />
+            <Route
+              path="/hospitals-enrolment"
+              element={<HospitalsEnrolment />}
+            />
+            <Route path="/specialists" element={<Specialists />} />
+            <Route path="/specialist/:userId" element={<SpecialistDetails />} />
+            <Route path="/hospital/:userId" element={<Hospital />} />
+            {/* </Route> */}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './store.js'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from './store.js';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <main className="nunito-sans text-[13px]" style={{ fontFamily: '12px' }}>
+        <Toaster />
+        <App />
+      </main>
     </React.StrictMode>
   </Provider>
-)
+);
