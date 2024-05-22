b914ea5515216fe345677d2caa89e7fb31cb71ed
Okpainmo <okpainmoandrew@gmail.com>
2024-05-22 09:59:56 +0100

    chore: still attempting the deployment bug fix

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_BASE_URL,
+  baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
   credentials: "include",
 });
 
