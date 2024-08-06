d06b06ed46f4c2750c94fd3b3b33a6252e1bc918
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-08-06 06:32:12 +0100

    refactor: setting up redux for modal function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-// import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import GlobalModal from './components/GlobalModal';
+
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import ForgotPasswordMessage from './pages/ForgotPasswordMessage';
-// import Layout from './components/Layout';
 import Home from './pages/Home';
 import Admins from './pages/Admins';
 import Specialists from './pages/Specialists';
@@ -11,43 +13,46 @@ import Hospitals from './pages/Hospitals';
 import SpecialistDetails from './components/SpecialistDetails';
 import Hospital from './components/Hospital';
 import NotFound from './pages/NotFound';
-// import PrivateRoute from './components/PrivateRoute';
-// import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import CreateAdmin from './pages/Admins/components/CreateAdmin';
 import JobsPage from './pages/Jobs';
 import HospitalsEnrolment from './pages/HospitalsEnrolment';
 import UpdateAdmin from './pages/Admins/components/UpdateAdmin';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="/forgot-password-message"
-          element={<ForgotPasswordMessage />}
-        />
-
-        {/* <Route path="/" element={<Layout />}> */}
-        {/* <Route path="" element={<PrivateRoute />}/> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/admins" element={<Admins />} />
-        <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/admins/create-admin-account" element={<CreateAdmin />} />
-        <Route path="/admins/update-admin-account" element={<UpdateAdmin />} />
-        <Route path="/hospitals" element={<Hospitals />} />
-        <Route path="/hospitals-enrolment" element={<HospitalsEnrolment />} />
-        <Route path="/specialists" element={<Specialists />} />
-        <Route path="/specialist/:userId" element={<SpecialistDetails />} />
-        <Route path="/hospital/:userId" element={<Hospital />} />
-        <Route path="*" element={<NotFound />} />
-        {/* </Route> */}
-        {/* </Route> */}
-      </Routes>
-      {/* <ToastContainer /> */}
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            path="/forgot-password-message"
+            element={<ForgotPasswordMessage />}
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/admins" element={<Admins />} />
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route
+            path="/admins/create-admin-account"
+            element={<CreateAdmin />}
+          />
+          <Route
+            path="/admins/update-admin-account"
+            element={<UpdateAdmin />}
+          />
+          <Route path="/hospitals" element={<Hospitals />} />
+          <Route path="/hospitals-enrolment" element={<HospitalsEnrolment />} />
+          <Route path="/specialists" element={<Specialists />} />
+          <Route path="/specialist/:userId" element={<SpecialistDetails />} />
+          <Route path="/hospital/:userId" element={<Hospital />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <GlobalModal />
+        {/* <ToastContainer /> */}
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -1,17 +1,18 @@
-import { configureStore} from "@reduxjs/toolkit";
-// import authReducer from './slices/authSlice'
-import dashboardReducer from './slices/dashboardSlice'
-import usersSliceReducer from './slices/usersSlice'
-import {apiSlice} from './slices/apiSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import dashboardReducer from './slices/dashboardSlice';
+import usersSliceReducer from './slices/usersSlice';
+import { apiSlice } from './slices/apiSlice';
+import modalReducer from './slices/modalSlice';
+
 const store = configureStore({
   reducer: {
-    // auth: authReducer,
     dashboard: dashboardReducer,
-    users:usersSliceReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer
-  },
+    users: usersSliceReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    modal: modalReducer
+  }
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   // devTools: true,
 });
 
-export default store
+export default store;
