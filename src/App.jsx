5238771a470ccb03df79f3ea40e670ab7855ddbb
Okpainmo <okpainmoandrew@gmail.com>
2024-11-05 11:06:01 +0100

    chore: implemented route protection + more

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import store from './store';
 import GlobalModal from './components/GlobalModal';
-import axios from 'axios';
+// import axios from 'axios';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import PasswordResetMessage from './pages/PasswordResetMessage';
@@ -19,7 +19,8 @@ import UpdateAdmin from './pages/Admins/components/UpdateAdmin';
 import Specialist from './pages/Specialist';
 import Hospital from './pages/Hospital';
 import SingleJob from './pages/Jobs/components/job';
-import { getUserInfo, getAccessToken, storeUserInfo } from './utils/tokenUtils';
+import PrivateRoute from './components/PrivateRoute';
+// import { getUserInfo, getAccessToken, storeUserInfo } from './utils/tokenUtils';
 // import { ToastContainer } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
@@ -31,48 +32,48 @@ function App() {
   S3 pre-signer SDK was used, and all file URLs will expire after about 7 days.
   */
 
-  const userInfo = getUserInfo();
-  const token = getAccessToken();
+  // const userInfo = getUserInfo();
+  // const token = getAccessToken();
 
-  console.log('userInfo outside', userInfo);
+  // console.log('userInfo outside', userInfo);
 
-  if (userInfo && userInfo.profileImageData) {
-    console.log('userInfo inside', userInfo);
+  // if (userInfo && userInfo.profileImageData) {
+  //   console.log('userInfo inside', userInfo);
 
-    const imageTimeToLive = userInfo.profileImageData.lastFetch + 590400000;
+  //   const imageTimeToLive = userInfo.profileImageData.lastFetch + 590400000;
 
-    if (imageTimeToLive < Date.now()) {
-      const handleUpdateUserProfileImage = async () => {
-        const response = await axios.get(
-          `http://localhost:3001/api/v1/admin/get-admin-profile/${userInfo.userId}`,
-          {
-            withCredentials: true,
-            headers: {
-              Authorization: `Bearer ${token}`,
-              Email: userInfo.email
-            }
-          }
-        );
+  //   if (imageTimeToLive < Date.now()) {
+  //     const handleUpdateUserProfileImage = async () => {
+  //       const response = await axios.get(
+  //         `http://localhost:3001/api/v1/admin/get-admin-profile/${userInfo.userId}`,
+  //         {
+  //           withCredentials: true,
+  //           headers: {
+  //             Authorization: `Bearer ${token}`,
+  //             Email: userInfo.email
+  //           }
+  //         }
+  //       );
 
-        if (response.status === 200) {
-          // console.log(response);
+  //       if (response.status === 200) {
+  //         // console.log(response);
 
-          const updatedUserInfo = {
-            ...userInfo,
-            profileImageData: response.data.response.adminProfile.profileImage
-          };
+  //         const updatedUserInfo = {
+  //           ...userInfo,
+  //           profileImageData: response.data.response.adminProfile.profileImage
+  //         };
 
-          storeUserInfo(updatedUserInfo);
+  //         storeUserInfo(updatedUserInfo);
 
-          // console.log('user profile image updated', updatedUserInfo);
-        }
-      };
+  //         // console.log('user profile image updated', updatedUserInfo);
+  //       }
+  //     };
 
-      handleUpdateUserProfileImage();
-    } else {
-      // console.log('userProfileImage not expired', userInfo);
-    }
-  }
+  //     handleUpdateUserProfileImage();
+  //   } else {
+  //     // console.log('userProfileImage not expired', userInfo);
+  //   }
+  // }
 
   return (
     // <Provider store={store}>
@@ -85,22 +86,29 @@ function App() {
           path="/password-reset-message"
           element={<PasswordResetMessage />}
         />
-        <Route path="/" element={<Home />} />
-        <Route path="/admins" element={<Admins />} />
-        <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/admins/create-admin-account" element={<CreateAdmin />} />
-        <Route
-          path="/admins/update-admin-account/:adminId"
-          element={<UpdateAdmin />}
-        />
-        <Route path="/hospitals" element={<Hospitals />} />
-        <Route path="/hospitals-enrolment" element={<HospitalsEnrolment />} />
-        <Route path="/specialists" element={<Specialists />} />
-        <Route path="/specialist/:specialistId" element={<Specialist />} />
-        <Route path="/hospital/:hospitalId" element={<Hospital />} />
-        <Route path="/jobs/job/:jobId" element={<SingleJob />} />
+
         {/* <Route path="/hospital/:userId" element={<Hospital />} /> */}
         <Route path="*" element={<NotFound />} />
+
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/admins" element={<Admins />} />
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route
+            path="/admins/create-admin-account"
+            element={<CreateAdmin />}
+          />
+          <Route
+            path="/admins/update-admin-account/:adminId"
+            element={<UpdateAdmin />}
+          />
+          <Route path="/hospitals" element={<Hospitals />} />
+          <Route path="/hospitals-enrolment" element={<HospitalsEnrolment />} />
+          <Route path="/specialists" element={<Specialists />} />
+          <Route path="/specialist/:specialistId" element={<Specialist />} />
+          <Route path="/hospital/:hospitalId" element={<Hospital />} />
+          <Route path="/jobs/:jobId" element={<SingleJob />} />
+        </Route>
       </Routes>
       <GlobalModal />
       {/* <ToastContainer /> */}
